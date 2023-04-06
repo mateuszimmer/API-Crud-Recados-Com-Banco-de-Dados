2e01e29f90b1261e5342ca3b74ffbfececccc201
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-04-06 16:52:42 -0300

    Teste delete-recado.usecase

---
+++ tests/app/features/recados/usecases/delete-recado.usecase.spec.ts
@@ -0,0 +1,70 @@
+import { RecadoRepository } from "../../../../../src/app/features/recados/repositories/recado.repository";
+import { RedisConnection } from "../../../../../src/main/database/redis.connection";
+import { DatabaseConnection } from "../../../../../src/main/database/typeorm.connections";
+import { DeletarRecadoUseCase } from '../../../../../src/app/features/recados/usecases/delete-recado.usecase';
+import { CacheRepository } from "../../../../../src/app/shared/database/repositories/cache.repository";
+
+describe('Testes DeletarRecadoUseCase', () => {
+
+    beforeAll(async () => {
+        await DatabaseConnection.connect();
+        await RedisConnection.connect();
+    });
+
+    afterAll(async () => {
+        await DatabaseConnection.destroy();
+        await RedisConnection.destroy();
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const makeSut = () => {
+        const repository = new RecadoRepository();
+        const sut = new DeletarRecadoUseCase(repository);
+        
+        return {
+            sut,
+            repository
+        }
+    };
+
+    test('Deve chamar o método Delete do Repository com os parâmtros corretos', async () => {
+        const { sut, repository } = makeSut();
+        const id = "id-existente";
+        const usuario = "id-válido";
+        
+        jest.spyOn(RecadoRepository.prototype, 'delete').mockResolvedValue();
+
+        await sut.execute(id, usuario)
+
+        expect(repository.delete).toHaveBeenCalledTimes(1)
+        expect(repository.delete).toHaveBeenCalledWith(id, usuario)
+    })
+
+    test('Deve retornar a resposta da exclusão { raw: [], affected: 0 } se o recado não for localizado', async () => {
+        const idInvalido = "id-não-existente";
+        const usuario = "id-válido";
+        const { sut } = makeSut();
+
+        jest.spyOn(RecadoRepository.prototype, 'delete').mockResolvedValue({ raw: [], affected: 0 });
+
+        const resposta = await sut.execute(idInvalido, usuario)
+
+        expect(resposta?.affected).toBe(0)        
+    })
+
+    test('Deve retornar a resposta da exclusão { raw: [], affected: 1 } se o recado for localizado e deletado', async () => {
+        const id = "id-existente";
+        const usuario = "id-válido";
+        const { sut } = makeSut();
+    
+        jest.spyOn(RecadoRepository.prototype, 'delete').mockResolvedValue({ raw: [], affected: 1 })
+
+        const resposta = await sut.execute(id, usuario)
+
+        expect(resposta?.affected).toBe(1)        
+    })
+
+})
